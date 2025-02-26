686d05c59e0b5bab27cfe078fe9eaf27b02a6dfa
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-02-26 15:41:47 +0500

    firsto33

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 3000;
 app.use(express.json())
 app.use(cors(
     {
-        origin : "http://localhost:5173",
+        origin : "*",
         "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
     }
 ))
