21d1c24e40d73ec36b69e81c03db4f81a61e7e2c
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-02-26 15:50:09 +0500

    solved

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 3000;
 app.use(express.json())
 app.use(cors(
     {
-        origin : "*",
+        origin : "https://google-form-chi-ashy.vercel.app",
         "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
     }
 ))
