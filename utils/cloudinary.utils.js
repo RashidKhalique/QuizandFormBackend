846655529394d6978fe3847fb0672e3299037acc
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-02-27 10:53:08 +0500

    uploadeew

--- utils/cloudinary.utils.js
+++ utils/cloudinary.utils.js
@@ -16,7 +16,7 @@ cloudinary.config({
         const uploadOnCloudinary = async (localFilePath) =>{
             try {
                 if(!localFilePath) return null;
-               const response =await cloudinary.uploader.upload(localFilePath, {
+               const response = await cloudinary.uploader.upload(localFilePath, {
                     resource_type : "auto"
                 })
                 console.log("File uploade Successfully", response.url);
