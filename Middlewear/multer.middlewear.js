564c622b79340da7bfab498a91f3803da992122d
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-02-27 16:34:06 +0500

    uploaded solvins

--- Middlewear/multer.middlewear.js
+++ Middlewear/multer.middlewear.js
@@ -1,16 +1,34 @@
-import multer from "multer";
+// import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname + '-' + Date.now())
-    }
-  })
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './public/temp')
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.originalname + '-' + Date.now())
+//     }
+//   })
   
-export const upload = multer({ storage: storage })
+// export const upload = multer({ storage: storage })
+// import multer from "multer";
+import multer from "multer";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log('Saving to:', './public/temp');
+    cb(null, './public/temp');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
+
+
+
+
+
+// - Rashid Khalique (Mern Stack Modification Project Multer middlewear) 
+// - All are Modified of Midddlewear Folder
 
 
 
